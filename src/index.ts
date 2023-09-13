bd82e4b55a01cfd03a2251576fd06875444493aa
maccioni <joaovmdd@gmail.com>
2023-09-12 21:50:57 -0300

    feat: my second project to my friend Miguel

--- src/index.ts
+++ src/index.ts
@@ -10,52 +10,25 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
+app.get("/users", async (req, res) => {
+  const todos = await prisma.user.findMany({
     orderBy: { createdAt: "desc" },
   });
 
   res.json(todos);
 });
 
-app.post("/todos", async (req, res) => {
-  const todo = await prisma.todo.create({
+app.post("/user", async (req, res) => {
+  const { name, email, } = req.body
+
+  const todo = await prisma.user.create({
     data: {
-      completed: false,
-      createdAt: new Date(),
-      text: req.body.text ?? "Empty todo",
+      name,
+      email
     },
   });
 
   return res.json(todo);
-});
-
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
-    where: { id },
-  });
-
-  return res.json(todo);
-});
-
-app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
-
-  return res.json(todo);
-});
-
-app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
-
-  return res.send({ status: "ok" });
 });
 
 app.get("/", async (req, res) => {
